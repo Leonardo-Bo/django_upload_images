e74d8ffebecb470237bb90159372d3d538cd3a34
leonardo <boleo88@gmail.com>
2021-02-23 14:34:23 +0100

    built TextModel-ImageModel and started building templates

--- textimage/apps/item/models.py
+++ textimage/apps/item/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+def image_upload_to(instance, filename):
+    return "{}/{}".format(instance.textmodel.pk, filename)
+
+
+class TextModel(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Title", unique=True)
+    content = models.TextField(verbose_name="Content")
+
+    def __str__(self):
+        return self.title
+
+
+class ImageModel(models.Model):
+    reftext = models.ForeignKey(TextModel, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=image_upload_to)
+
+    def delete(self, *args, **kwargs):
+        self.image.delete()
+        super().delete(*args, **kwargs)
